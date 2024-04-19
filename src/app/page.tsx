2212779cbd0d2629e360c8275f22123a258832a9
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2024-04-19 19:24:34 -0300

    feat: add welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import Header from "@/layout/header/header";
-
+import { format } from "date-fns";
+import { ptBR } from 'date-fns/locale';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <div className="py-6 px-5">
+        <h2 className="text-xl ">Olá, user</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
+
 
     </main>
   );
