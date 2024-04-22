4f2e83b9b985c53d2e0a1ea78c4b65c7acc96a51
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2024-04-22 16:18:39 -0300

    feat: add auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/layout/header/header";
 import Footer from "@/layout/footer/footer";
+import Providers from "@/context/providers/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} dark`}>
-        <Header />
-        {children}
-        <Footer />
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
