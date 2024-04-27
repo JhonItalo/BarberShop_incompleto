d1b067a6cb50e7a559e60b008a7a94592709cdfc
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2024-04-27 16:30:17 -0300

    styles: add responsividade no footer

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-secondary py-6 px-5 flex justify-center items-center">
+        <footer className="w-full bg-secondary py-6 px-5 flex justify-center items-center
+        1xl:max-w-[1440px] 1xl:mx-auto
+        ">
             <p className="text-gray-400 text-xs font-bold opacity-75">© 2023 Copyright FSW Barber</p>
         </footer>
     )
