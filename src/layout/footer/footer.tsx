61dfa98c01bb1193ca50b659ccc9604a4ed7a379
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2024-04-27 10:49:10 -0300

    styles: modificando footer

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-secondary py-6 px-5">
+        <footer className="w-full bg-secondary py-6 px-5 flex justify-center items-center">
             <p className="text-gray-400 text-xs font-bold opacity-75">© 2023 Copyright FSW Barber</p>
         </footer>
     )
