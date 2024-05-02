4e603f8475369bf2e6a89804e6eb0002044839b9
John Ítalo Lucas Lima <jhon2italo@gmail.com>
2024-05-02 17:43:12 -0300

    feat: add page search

---
+++ src/app/(private)/search/[id]/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+import { useSearchParams } from 'next/navigation'
+
+export default function SearchPage() {
+    const searchParams = useSearchParams()
+
+    return (
+        <div>teste</div>
+    )
+}
